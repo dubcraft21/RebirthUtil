c142d198ef119ccd5d6711661fe73f817ec00b5a
dubcraft21 <dubcraft21@gmail.com>
2014-10-08 22:51:17 -0700

    bound proxies to main class

--- main/java/dubcraft21/main/Main.java
+++ main/java/dubcraft21/main/Main.java
@@ -4,11 +4,17 @@ import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.registry.GameRegistry;
 import dubcraft21.command.ClientCommandManager;
 
+
 @Mod(modid = "rebirthUtil")
 public class Main {
+	
+	@SidedProxy(clientSide = "dubcraft21.main.ClientProxy", serverSide = "dubcraft21.main.ServerProxy")
+	public static ServerProxy proxy;
+	
 	public ClientCommandManager clientCMDM;
 	public Main() {
 		clientCMDM = new ClientCommandManager();
